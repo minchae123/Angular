fc67e8bb3073a089da0b844543113ed976a7e1f5
minchae123 <08minchae17@gmail.com>
2023-01-09 18:47:29 +0900

    로직

---
+++ ColorMatch/Assets/01_Scripts/Rotation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Rotation : MonoBehaviour
+{
+    public Transform tr;
+
+    public float turnAngle = 90;    // Angle to turn in degrees;
+    public float angle;
+
+    public void Left()
+    {
+        angle += -turnAngle;
+        transform.DORotate(new Vector3(0, 0, angle), 1);
+    }
+
+    public void Right()
+    {
+        angle += turnAngle;
+        transform.DORotate(new Vector3(0, 0, angle), 1);
+    }
+}
