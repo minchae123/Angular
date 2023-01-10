82346f22d8ea330dd765218834b2e8b4f6028dbf
minchae123 <08minchae17@gmail.com>
2023-01-10 14:50:26 +0900

    ㅇㅗㅂㅓ UI

--- ColorMatch/Assets/01_Scripts/UIManager.cs
+++ ColorMatch/Assets/01_Scripts/UIManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
@@ -10,6 +12,8 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI overScoreTxt;
     public TextMeshProUGUI overBestScoreTxt;
 
+    public GameObject overCanvas;
+
     private void Start()
     {
         scoreTxt.text = GameManager.instance.score.ToString();
@@ -17,11 +21,36 @@ public class UIManager : MonoBehaviour
 
     private void Update()
     {
-        scoreTxt.text = GameManager.instance.score.ToString();
+        scoreTxt.text = GameManager.instance.score.ToString() + " 점";
         overScoreTxt.text = $"점수 : {GameManager.instance.score.ToString()}" ;
-        overBestScoreTxt.text = GameManager.instance.bestScore.ToString();
+        overBestScoreTxt.text = $"최고 점수 : {GameManager.instance.bestScore.ToString()}";
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            UpSlide();
+        }
     }
 
+    public void UpSlide()
+    {
+        overCanvas.transform.DOLocalMove(new Vector3(0, 0, 90), 2);
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("빠이");
+        Application.Quit();
+    }
+
+    public void RestartGame(int n)
+    {
+        SceneManager.LoadScene(n);
+    }
+
+    public void GoTitle(int n)
+    {
+        SceneManager.LoadScene(n);
+    }
 
 
 }
